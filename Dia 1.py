b41a1659ea4df4e3f7f28ad6cee775b936ad1119
Ana Carolina Adriano de Assis <97900395+anacassis@users.noreply.github.com>
2022-04-19 10:01:26 -0300

    Add files via upload

---
+++ Dia 1.py
@@ -0,0 +1,21 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
+
+print("Olá sejam bem vindos ao Programa de Geração de Nome de Bandas ")
+print(" - Primeira Pergunta:")
+street = input(" -     Em Qual cidade você cresceu?")
+cor = input(" -     Agora me fala a cor que você mais curte:")
+animal_estimacao = input(" -     Agora me fale um nome de animal de estimação:")
+nome_banda = print(" O nome da sua banda poderia ser" + street + animal_estimacao + cor +'.')
+
+
+
+
