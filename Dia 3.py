07a5c2dbca696e24be4503982aa191ebd4591c7a
Ana Carolina Adriano de Assis <97900395+anacassis@users.noreply.github.com>
2022-04-20 20:37:56 -0300

    Add files via upload

---
+++ Dia 3.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+choice1 = input('Qual caminho você quer serguir ? "direita" ou "esquerda" ?\n').lower()
+
+
+if choice1 == "esquerda":
+  choice2 = input("voce deseja Esperar um barco ou ir Nadando ?\n").lower()
+  if choice2 == "esperar":
+    choice3 = input("Qual porta voce deseja entrar? Amarelo, Azul ou Vermelho?\n").lower()
+    if choice3 == "Amarelo":
+      print("Você conseguiu achar o tesouro !!! Você Ganhou!!!") 
+    elif choice3 == "Vermelho":
+      print("Não foi dessa vez, Game Over")
+    elif choice3 == "Azul":
+      print("Não foi dessa vez, Game Over")
+    else:
+       print("Você escolheu uma cor que não existe")
+else:
+  print("Game over")
